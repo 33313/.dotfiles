dc7293cf8555e3eef562131b7f82ee2cccf8b790
33313 <84045642+33313@users.noreply.github.com>
2025-02-14 12:59:51 +0100

    refactor: remove lsp-zero, turn off checker notifs

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -1,19 +1,13 @@
 return {
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v4.x',
-        lazy = true,
-        config = false,
-    },
-    {
         'williamboman/mason.nvim',
         lazy = false,
-        config = true,
+        opts = {},
     },
     {
         'hrsh7th/nvim-cmp',
+        lazy = false,
         event = 'InsertEnter',
-
         dependencies = {
             { 'L3MON4D3/LuaSnip' },
             { 'hrsh7th/cmp-buffer' },
@@ -52,33 +46,38 @@ return {
     {
         'neovim/nvim-lspconfig',
         cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-
         event = { 'BufReadPre', 'BufNewFile' },
         dependencies = {
             { 'hrsh7th/cmp-nvim-lsp' },
             { 'williamboman/mason.nvim' },
             { 'williamboman/mason-lspconfig.nvim' },
         },
+        init = function()
+            vim.opt.signcolumn = 'yes'
+        end,
         config = function()
-            local lsp_zero = require('lsp-zero')
-            local lsp_attach = function(_, bufnr)
-                local opts = { buffer = bufnr, remap = false }
-                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-            end
+            local lsp_defaults = require('lspconfig').util.default_config
 
-            lsp_zero.extend_lspconfig({
-                sign_text = true,
-                lsp_attach = lsp_attach,
-                capabilities = require('cmp_nvim_lsp').default_capabilities()
+            lsp_defaults.capabilities = vim.tbl_deep_extend(
+                'force',
+                lsp_defaults.capabilities,
+                require('cmp_nvim_lsp').default_capabilities()
+            )
+            vim.api.nvim_create_autocmd('LspAttach', {
+                desc = 'LSP actions',
+                callback = function(_, bufnr)
+                    local opts = { buffer = bufnr, remap = false }
+                    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+                    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+                    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+                    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+                    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+                end,
             })
 
             require('mason-lspconfig').setup({
@@ -98,9 +97,23 @@ return {
                     function(server_name)
                         require('lspconfig')[server_name].setup({})
                     end,
-                    lua_ls = function()
-                        local lua_opts = lsp_zero.nvim_lua_ls()
-                        require('lspconfig').lua_ls.setup(lua_opts)
+                    biome = function()
+                        require('lspconfig').biome.setup({
+                            root_dir = function(fname)
+                                return require('lspconfig.util').root_pattern('package.json', 'tsconfig.json')(fname) or vim.loop.cwd()
+                            end,
+                        })
+                    end,
+                    ts_ls = function()
+                        require('lspconfig').ts_ls.setup({
+                            handlers = {
+                                ["textDocument/publishDiagnostics"] = function() end,
+                            },
+                            on_attach = function(client)
+                                client.server_capabilities.document_formatting = false
+                                client.server_capabilities.document_range_formatting = false
+                            end,
+                        })
                     end,
                 }
             })
