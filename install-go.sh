09a10ac3c915817f852fd33c25975300fc11a77b
33313 <84045642+33313@users.noreply.github.com>
2025-08-09 04:41:03 +0200

    refactor: separate go installation script

---
+++ install-go.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Install Go
+# Download latest
+GO_VERSION=$(curl -s https://go.dev/VERSION?m=text | head -n 1 | sed 's/go//')
+GO_DOWNLOAD_URL="https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
+wget -q --show-progress -O go.tar.gz "$GO_DOWNLOAD_URL"
+
+# Remove old, unpack new
+rm -rf "/usr/local/go/bin"
+tar -C "/usr/local" -xzf go.tar.gz
+rm go.tar.gz
+
+# Add Go to PATH if not already present
+if ! grep -q "export PATH=/usr/local/go/bin:\$PATH" ~/.bashrc; then
+    echo "export PATH=/usr/local/go/bin:\$PATH" >> ~/.bashrc
+fi
+
+export PATH="/usr/local/go/bin:$PATH"
