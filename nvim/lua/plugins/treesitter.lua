f65686eba8fd6dd6e7dce6e8142865faf9ee45e8
33313 <84045642+33313@users.noreply.github.com>
2025-09-11 22:48:52 +0200

    refactor(treesitter): BufReadPost -> BufEnter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -39,7 +39,7 @@ return {
             "css",
             "tsx"
         })
-        vim.api.nvim_create_autocmd("BufReadPost", {
+        vim.api.nvim_create_autocmd("BufEnter", {
             pattern = "*",
             callback = function()
                 pcall(vim.treesitter.start) -- errors for ft with no parser
